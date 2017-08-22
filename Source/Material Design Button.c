4124627a7b04a7689ced2839d9b2d483328cab51
Pengyi Mu <mupengyi@foxmail.com>
2017-08-22 17:55:39 +0800

    Update Material Design Button.c

--- Source/Material Design Button.c
+++ Source/Material Design Button.c
@@ -1,12 +1,10 @@
 #include <windows.h>
-#include <stdlib.h>
-
 
 LRESULT CALLBACK WndProc(HWND,UINT,WPARAM, LPARAM); 
 
 int WINAPI WinMain(HINSTANCE hInstance,HINSTANCE hPrevInstance,LPSTR lpCmdLine,int nCmdShow)
 {
-	static TCHAR szAppName[] = TEXT("Windows程序");
+	static TCHAR szAppName[] = TEXT("Windows API Material Design");
 	MSG msg;
 	WNDCLASSEX wc;
 	HWND hwnd;
@@ -25,7 +23,7 @@ int WINAPI WinMain(HINSTANCE hInstance,HINSTANCE hPrevInstance,LPSTR lpCmdLine,i
 		MessageBox(NULL,TEXT("很抱歉，注册窗口类失败!"),TEXT("警告"),MB_OK);
 		return 0;
 	}
-	hwnd = CreateWindowEx(WS_EX_CLIENTEDGE,szAppName,TEXT("Material Design Button"),WS_VISIBLE|WS_OVERLAPPEDWINDOW,CW_USEDEFAULT,CW_USEDEFAULT,500,200,NULL,NULL,hInstance,NULL);
+	hwnd = CreateWindowEx(WS_EX_DLGMODALFRAME,szAppName,TEXT("Material Design Button"),WS_SIZEBOX|WS_VISIBLE|WS_OVERLAPPEDWINDOW,CW_USEDEFAULT,CW_USEDEFAULT,500,200,NULL,NULL,hInstance,NULL);
 	if(hwnd == NULL)
 	{
 		MessageBox(NULL, "Window Creation Failed!","Error!",MB_ICONEXCLAMATION|MB_OK);
@@ -49,11 +47,9 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT Message, WPARAM wParam, LPARAM lParam)
 	static HDC hdc;
 	static HPEN hPen,hPenOld;
 	static HBRUSH hBrush;
-	int random,cxtemp,cytemp;
-	static int i,Fast;				/*判断如何绘制*/
-	static int x,y; 				/*鼠标位置坐标*/
+	static int DecideToDraw,DecideToDrawSpeed;				/*判断如何绘制*/
+	static int x,y;					/*鼠标位置坐标*/
 	static int Radii;				/*圆形半径*/
-	POINT pt;
 	PAINTSTRUCT ps;
 	
 	static int cxClient,cyClient;
@@ -67,8 +63,8 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT Message, WPARAM wParam, LPARAM lParam)
 		}
 		case WM_LBUTTONDOWN:
 		{
-			i = TRUE;
-			Fast = FALSE;
+			DecideToDraw = TRUE;
+			DecideToDrawSpeed = FALSE;
 			Radii = 1;
 			x = LOWORD(lParam);
 			y = HIWORD(lParam);
@@ -78,8 +74,8 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT Message, WPARAM wParam, LPARAM lParam)
 		}
 		case WM_LBUTTONUP:
 		{
-			i = FALSE;
-			Fast = TRUE;
+			DecideToDraw = FALSE;
+			DecideToDrawSpeed = TRUE;
 			x = LOWORD(lParam);
 			y = HIWORD(lParam);
 			InvalidateRect(hwnd,NULL,TRUE);
@@ -103,21 +99,25 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT Message, WPARAM wParam, LPARAM lParam)
 		{
 			hdc = BeginPaint(hwnd,&ps);
 			SetBkMode(hdc,TRANSPARENT);
-			hBrush = CreateSolidBrush(RGB(200,200,200));
+			hBrush = CreateSolidBrush(RGB(190,190,190));
 			SelectObject(hdc,GetStockObject(NULL_PEN));
 			SelectObject(hdc,hBrush);
-			if(i)
+			if(DecideToDraw)
 			{
 				Ellipse(hdc,x-Radii,y-Radii,x+Radii,y+Radii);
 				Radii++;
+				if(Radii >= max(cxClient,cyClient))
+				{
+					DecideToDraw = FALSE;
+				}
 			}
-			else if(Fast)
+			else if(DecideToDrawSpeed)
 			{
 				Ellipse(hdc,x-Radii,y-Radii,x+Radii,y+Radii);
 				Radii += 3;
 				if(Radii >= max(cxClient,cyClient))
 				{
-					Fast = FALSE;
+					DecideToDrawSpeed = FALSE;
 				}
 				Sleep(3);
 				InvalidateRect(hwnd,NULL,TRUE);
